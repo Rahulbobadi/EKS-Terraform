772aa104203cfc584f94b6d8251c22b9717ed341
Rahul Bobadi <66925050+Rahulbobadi@users.noreply.github.com>
2026-04-15 13:52:35 +0530

    Updated 0-provider.tf
    Updated 0-provider.tf

--- 0-provider.tf
+++ 0-provider.tf
@@ -3,6 +3,14 @@ provider "aws" {
 }
 
 terraform {
+  backend "s3" {
+    bucket         = "my-eks-terraform-demo"   # 🔁 change to your bucket name
+    key            = "eks-demo/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
